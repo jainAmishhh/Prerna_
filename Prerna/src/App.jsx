0c8bb1d3fd6891c975f2b0f8843a27f06b644d0c
jainAmishhh <jainamish1511@gmail.com>
2025-11-19 00:54:00 +0530

    Integrate new About components and define main application routing/structure.

---
+++ Prerna/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AboutPrernaPlatform from './Components/AboutSection/AboutPrernaPlatform'
+
+const App = () => {
+  return (
+    <div>
+      <AboutPrernaPlatform />
+    </div>
+  )
+}
+
+export default App
